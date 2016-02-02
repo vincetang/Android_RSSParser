105de0bfecc4b59d7e7462e65a90ec197135203f
Vincent Tang <Vince@Vincents-MacBook-Pro.local>
2016-02-02 08:52:39 -0500

    updated code, removed floating button

--- app/src/main/java/com/vincetang/top10downloader/MainActivity.java
+++ app/src/main/java/com/vincetang/top10downloader/MainActivity.java
@@ -33,15 +33,6 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         btnParse = (Button) findViewById(R.id.btnParse);
         listApps = (ListView) findViewById(R.id.xmlListView);
 
